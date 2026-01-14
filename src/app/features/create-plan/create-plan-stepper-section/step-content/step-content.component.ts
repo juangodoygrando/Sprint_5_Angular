be96e19fb6eb59ddf451e160eb44de5f0d5c6fe2
Juan Godoy <juangodoygrando@gmail.com>
2026-01-14 19:39:08 +0100

    feat:creation of new components for 'create-plan-stepper-section'

---
+++ src/app/features/create-plan/create-plan-stepper-section/step-content/step-content.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-step-content',
+  imports: [],
+  templateUrl: './step-content.component.html',
+  styleUrl: './step-content.component.css'
+})
+export class StepContentComponent {
+
+}
---
+++ src/app/features/create-plan/create-plan-stepper-section/step-item/step-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-step-item',
+  imports: [],
+  templateUrl: './step-item.component.html',
+  styleUrl: './step-item.component.css'
+})
+export class StepItemComponent {
+
+}
