d79fc79f8c4ac75b30af7cead1d22e78a2424d6c
Juan Godoy <juangodoygrando@gmail.com>
2026-01-19 21:02:21 +0100

    REFACTOR: Change the 'Create your plan' button to be a reusable component and adapt the logic to handle input.STYLE: Increase the size of the logo and text in the header.

--- src/app/features/create-plan/create-plan-stepper-section/stepper/stepper.component.ts
+++ src/app/features/create-plan/create-plan-stepper-section/stepper/stepper.component.ts
@@ -1,18 +1,26 @@
-import { Component, inject } from '@angular/core';
-import { StepItemComponent } from "../step-item/step-item.component";
-import { StepContentComponent } from "../step-content/step-content.component";
+import { Component, computed, inject } from '@angular/core';
+import { StepItemComponent } from '../step-item/step-item.component';
+import { StepContentComponent } from '../step-content/step-content.component';
 import { StepperService } from '../../../../services/stepper.service';
-import { StepSummaryComponent } from "../step-summary/step-summary.component";
-import { ButtonSummaryComponent } from "../../components/button-summary/button-summary.component";
+import { StepSummaryComponent } from '../step-summary/step-summary.component';
+
+import { CoffeePlanService } from '../../../../services/coffee-plan.service';
+import { ButtonCreatePlanComponent } from '../../../../shared/components/button-create-plan/button-create-plan.component';
 
 @Component({
   selector: 'app-stepper',
-  imports: [StepItemComponent, StepContentComponent, StepSummaryComponent, ButtonSummaryComponent],
+  imports: [
+    StepItemComponent,
+    StepContentComponent,
+    StepSummaryComponent,
+    ButtonCreatePlanComponent,
+  ],
   templateUrl: './stepper.component.html',
   styleUrl: './stepper.component.css',
 })
 export class StepperComponent {
   stepperService = inject(StepperService);
+  coffeePlanService = inject(CoffeePlanService);
 
   private _steps = this.stepperService.stepperData();
 
@@ -22,4 +30,6 @@ export class StepperComponent {
   public set steps(value) {
     this._steps = value;
   }
+
+  disabled = computed(() => !this.coffeePlanService.isOrderComplete());
 }
---
+++ src/app/shared/components/button-create-plan/button-create-plan.component.ts
@@ -0,0 +1,11 @@
+import { Component ,input } from '@angular/core';
+
+@Component({
+  selector: 'button-create-plan',
+  imports: [],
+  templateUrl: './button-create-plan.component.html',
+  styleUrl: './button-create-plan.component.css',
+})
+export class ButtonCreatePlanComponent {
+  disabled = input<boolean>(false);
+}
