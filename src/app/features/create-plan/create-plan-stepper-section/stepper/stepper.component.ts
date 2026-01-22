29ed42c254ce315ea076f382d56f40c2b7fec750
Juan Godoy <juangodoygrando@gmail.com>
2026-01-22 20:21:36 +0100

    chore:naming change

--- src/app/features/create-plan/create-plan-stepper-section/stepper/stepper.component.ts
+++ src/app/features/create-plan/create-plan-stepper-section/stepper/stepper.component.ts
@@ -7,7 +7,7 @@ import { StepSummaryComponent } from '../step-summary/step-summary.component';
 import { CoffeePlanService } from '../../../../services/coffee-plan.service';
 import { ButtonCreatePlanComponent } from '../../../../shared/components/button-create-plan/button-create-plan.component';
 import { ModalComponent } from '../../../../shared/components/modal/modal.component';
-import { PricingService } from '../../../../services/Pricing.service';
+import { PricingService } from '../../../../services/pricing.service';
 
 
 
