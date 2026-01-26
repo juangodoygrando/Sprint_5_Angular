a860d50e7e2c8bac290a7c2905469e28665786ae
Juan Godoy <juangodoygrando@gmail.com>
2026-01-27 00:39:40 +0100

    feat: add layered text effect behind product images

--- src/app/features/home/home-our-collection-section/home-our-collection-section.component.ts
+++ src/app/features/home/home-our-collection-section/home-our-collection-section.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { collection } from '../../../shared/interfaces/ourCollection.interface';
+import { ourCollection } from '../../../data/ourCollection.data';
 
 @Component({
   selector: 'app-home-our-collection-section',
   imports: [],
   templateUrl: './home-our-collection-section.component.html',
-  styles: ``
+  styleUrl: './home-our-collection-section.component.css',
 })
 export class HomeOurCollectionSectionComponent {
-
+  ourCollection: collection[] = ourCollection;
 }
