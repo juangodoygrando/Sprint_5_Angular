8619b54b42361e3f22f6de7d8cdf105199799da2
Juan Godoy <juangodoygrando@gmail.com>
2026-01-27 02:10:00 +0100

    feat:Embed the 'steps-section' and 'button-create-plan' components to complete the home page

--- src/app/features/home/page/home-page/home-page.component.ts
+++ src/app/features/home/page/home-page/home-page.component.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import { HomeHeroSectionComponent } from "../../home-hero-section/home-hero-section.component";
 import { HomeOurCollectionSectionComponent } from "../../home-our-collection-section/home-our-collection-section.component";
 import { HomeWhyChooseUsSectionComponent } from "../../home-why-choose-us-section/home-why-choose-us-section.component";
+import { steps } from '../../../../data/how-it-work.data';
+import { step } from '../../../../shared/interfaces/create-plan-steps.interface';
+import { StepsSectionComponent } from "../../../../shared/components/steps-section/steps-section.component";
+import { ButtonCreatePlanComponent } from "../../../../shared/components/button-create-plan/button-create-plan.component";
 
 @Component({
   selector: 'app-home-page',
-  imports: [HomeHeroSectionComponent, HomeOurCollectionSectionComponent, HomeWhyChooseUsSectionComponent],
+  imports: [HomeHeroSectionComponent, HomeOurCollectionSectionComponent, HomeWhyChooseUsSectionComponent, StepsSectionComponent, ButtonCreatePlanComponent],
   templateUrl: './home-page.component.html',
   styles: ``
 })
 export class HomePageComponent {
-
+  stepArray: step[] = steps;
 }
